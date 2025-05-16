0d262edb856e026f1a14ec0a083a760f27d94413
60fHertz <133scylla@gmail.com>
2025-05-16 14:07:37 +0300

    wk 3 weekly code challenge

---
+++ sql_db_design/students_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE student (
+    name VARCHAR(100),
+    age INT,
+    gender VARCHAR(10)
+);
+
+-- Insert 3 unique records
+INSERT INTO student (name, age, gender)
+VALUES 
+    ('Alice Kimani', 21, 'Female'),
+    ('Brian Otieno', 23, 'Male'),
+    ('Cynthia Wanjiru', 22, 'Female');
